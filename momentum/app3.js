5fd3acbfe9391960112ee7b76302422aa99706ff
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-05-19 14:51:48 +0900

    20260519_Javascript-기초ifelse문, 연산자

--- momentum/app3.js
+++ momentum/app3.js
@@ -24,13 +24,19 @@ const age = parseInt( prompt("How old are you?") );
 // console.log(isNaN(age));
 // console.log(age, parseInt(age));
 
-if (isNaN(age)) { // boolean(true or false)값이 들어가야 한다.
-    // condition === true
-    console.log("Please write a number");
-} else {
-    // condition === false
-    console.log("Thank you for writing your age");
-}
+if (isNaN(age) || age < 0) { // boolean(true or false)값이 들어가야 한다.
+    console.log("Please write a real positive number");
+} else if(age < 18) {
+    console.log("You are too young.");
+} else if(age >= 18 && age <= 50) {
+    console.log("You can drink!");
+} else if(age > 50 && age <= 80) {
+    console.log("You should exercise!");
+} else if(age === 100) {
+    console.log("Wow you are wise.");
+} else if(age > 80) {
+    console.log("You can do whatever you want!");
+} 
 
 
 
