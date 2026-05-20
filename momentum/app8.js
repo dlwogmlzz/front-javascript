b9fde0d9871a62c5ae72007f6c563579b6a9a0f1
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-05-20 14:22:34 +0900

    20260520_Javascript - toggle기능으로 if-else에 담은 기능 한번에 처리하기

--- momentum/app8.js
+++ momentum/app8.js
@@ -4,7 +4,9 @@ function handleTitleClicked() {
     // const clickedClass = "clicked";
     // toggle기능으로 if문으로 하지 않고 한번에
     // toggle은 h1의 classList에 clicked class가 이미 있는지 확인해서
-    // 만약 있다면, toggle이 clicked를 제거해준다. 
+    // 만약 있다면, toggle이 clicked를 제거해준다.
+    // 만약에 h1의 classList에 clicked가 존재하지 않다면,
+    // toggle은 clicked를 classList에 추가해준다.
     h1.classList.toggle("clicked"); // 변수로 안만들고 "clicked"한번만 적으면 됨.
 }
 
