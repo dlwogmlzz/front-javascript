185bcbfc5b5114ffb8d234c8d654233867a0e36b
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-05-20 13:51:50 +0900

    20260520_Javascript - toggle기능으로 if-else에 담은 기능 한번에 처리하기

--- momentum/app7.js
+++ momentum/app7.js
@@ -1,12 +1,24 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
+// function handleTitleClick() {
+//     // h1.className = "active";
+//     const clickedClass = "clicked sexy-font";
+//     if (h1.className === clickedClass) {
+//         h1.className = "";
+//     } else {
+//         h1.className = clickedClass;
+//     }
+//     // console.log(h1.className);
+// }
+
 function handleTitleClick() {
-    // h1.className = "active";
-    const clickedClass = "clicked sexy-font";
-    if (h1.className === clickedClass) {
-        h1.className = "";
+    const clickedClass = "clicked";
+    // 명시된 class가 classList.contains - HTML element의 class에 포함되어 있는지 알려줌.
+    // 만약에 clickedClass가 h1의 classList에 포함되어 있지않으면..
+    if (h1.classList.contains(clickedClass)) {
+        h1.classList.remove(clickedClass);
     } else {
-        h1.className = clickedClass;
+        h1.classList.add(clickedClass);
     }
     // console.log(h1.className);
 }
