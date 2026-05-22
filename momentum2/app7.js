67dceebbe5139b2860adf86f9350f85e25bfa51b
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-05-22 10:04:10 +0900

    20260521_Javascript - localStorage를 이용한 입력 데이터 표시

--- momentum2/app7.js
+++ momentum2/app7.js
@@ -1,3 +1,12 @@
+// 1. 유저가 처음 앱을 열면 form과 h1은 CSS에 있는 hidden이라는 class때문에, 숨겨져 있다.
+// 2. 그 다음 자바스크립트가 USERNAME_KEY를 가지고 local storage를 확인함.
+// 3. 하지만 대부분의 경우에는 앱을 처음 실행했을 때, key랑 value가 없다.
+// 4. 즉 savedUsername값이 null이다.
+// 5. if (savedUsername === null) { 이 조건이 참이면 form에서 HIDDEN_CLASSNAME을 지워지면
+//    form이 표시되고 그다음 addEventListener가 form이 submit이 되기를 기다리고 있고,
+//    submit이 발생하면 onLoginSubmit 함수를 호출한다.
+// 6. onLoginSubmit 이 함수는 자바스크립트가 호출함.    
+
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
@@ -8,12 +17,12 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(yap_event) {
     yap_event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreetings();
 }
 
-function paintGreetings(username) {
+function paintGreetings() {
+    const username = localStorage.getItem(USERNAME_KEY);
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -32,5 +41,5 @@ if (savedUsername === null) {
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     // show the greetings
-    paintGreetings(savedUsername);
+    paintGreetings();
 }
