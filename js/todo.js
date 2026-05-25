409b611e5708441a69bc482985fc0f92c852db00
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-05-25 14:15:51 +0900

    20260525_Javascript - + todoList, 삭제 기능 추가

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,5 @@
+// ※※버튼이 클릭되었다는 것을 뭘로 어떻게 확인할까? "eventListener"로 확인함..
+
 // HTML에서 id가 todo-form인 form태그를 찾는다.
 const toDoForm = document.getElementById("todo-form");
 // 1. toDpInput을 전체HTML document에서 찾아볼 수 있다.
@@ -6,15 +8,43 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+// todoList삭제 함수..
+function deleteToDo(yap_event) {
+    // 옛날과 다르게 콘솔에서 path를 찾아볼수가 없음..
+    // 그래서 인자를 확인할때, 콘솔로그로 event.composedPath()라고 붙여줘야 됨..
+    // 옛날은 mouseEvent라 떴지만, 지금은 pointerEvent라고 뜬다.
+    // console.log(yap_event.composedPath());
+    // console.log(yap_event.target);
+    // console.dir(yap_event.target);
+    
+    // 부모의 정보를 얻을때.. , 어떤 버튼인지 innerText로 확인가능...
+    // console.dir(yap_event.target.parentElement.innerText);
+    // console.log(yap_event.target.parentElement);
+
+    // delete Todo..
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 // todo를 만드는 작업 함수..
 function paintToDo(newTodo) { // newTodo인자 넣음.
     // console.log("I will paint", newTodo);
+    // 1. li/span을 만들고, span의 텍스트를 변경.
     const li = document.createElement("li");
     const span = document.createElement("span");
-
-    // li를 span으로 자식으로 만듦.
-    li.appendChild(span);
     span.innerText = newTodo;
+
+    // 여기서 문제점..
+    // list에 item들을 추가할 수는 있지만, 지울 수는 없다는 것과 페이지를 새로고침하면, todoList들이 사라진다는것...
+
+    // 2. todoList를 삭제하는 버튼 추가.
+    const button = document.createElement("button");
+    button.innerText = "✘";
+    button.addEventListener("click", deleteToDo);
+
+    // 3. li요소안에 span, button을 자식요소로 추가.(append는 항상 맨 마지막에..)
+    li.appendChild(span)
+    li.appendChild(button);
     // console.log(li);
     toDoList.appendChild(li);
 }
@@ -35,7 +65,6 @@ function handleToDoSubmit(yap_event) {
 
     // paintTodo 함수를 호출하고, newTodo인자값을 넘김.
     paintToDo(newTodo);
-
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
