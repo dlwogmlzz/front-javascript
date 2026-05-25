debcc6c6c481fa040d5d95919f479ce680dc3bd5
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-05-25 10:51:27 +0900

    20260525_Javascript - + 이미지 랜덤으로 화면에 띄우기.

---
+++ js/background.js
@@ -0,0 +1,11 @@
+const images = ["dawangam.jpeg", "Magdalena.jpeg", "Pragser.jpeg", "tagimahal.jpeg", "santorini.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+// console.log(chosenImage);
+
+const bgImage = document.createElement("img");
+
+// <img src="img/~~.jpg"/> 이거랑 같음...
+bgImage.src = `img/${chosenImage}`;
+console.log(bgImage);
+document.body.appendChild(bgImage);
